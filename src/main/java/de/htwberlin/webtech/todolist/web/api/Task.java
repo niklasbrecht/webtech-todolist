d4ecaac212fbef8f0cbf2afebb3bd10cc2b1642b
niklasbrecht <niklas.brecht@gmx.net>
2022-11-18 15:38:56 +0100

    Task returns now UserID only.

--- src/main/java/de/htwberlin/webtech/todolist/web/api/Task.java
+++ src/main/java/de/htwberlin/webtech/todolist/web/api/Task.java
@@ -52,8 +52,8 @@ public class Task {
         this.datum = datum;
     }
 
-    public UserEntity getBenutzer() {
-        return this.benutzer;         //prints out on page
+    public long getBenutzer() {
+        return benutzer.getId();         //prints out on page
     }
-    
+
 }
