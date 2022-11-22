fbe16814f7203530c2b6bd36dde56f31826e4229
niklasbrecht <niklas.brecht@gmx.net>
2022-11-22 18:11:31 +0100

    Updated Dependencies, added Keyfiles to application.yml

--- src/main/java/de/htwberlin/webtech/todolist/ToDoListApplication.java
+++ src/main/java/de/htwberlin/webtech/todolist/ToDoListApplication.java
@@ -1,8 +1,11 @@
 package de.htwberlin.webtech.todolist;
 
+import de.htwberlin.webtech.todolist.security.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class ToDoListApplication {
 
