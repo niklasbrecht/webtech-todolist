bd5eea3daea0b0ba32fc76f99a1b205352c16a3a
niklasbrecht <niklas.brecht@gmx.net>
2023-01-01 20:47:37 +0100

    Backed Test for successful login

---
+++ src/test/java/de/htwberlin/webtech/todolist/web/AuthControllerTest.java
@@ -0,0 +1,63 @@
+package de.htwberlin.webtech.todolist.web;
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONString;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+    @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    String username = "test@gradle.com";
+    String password = "m7kP20nQbDRi";
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser(value = "test@gradle.com", password = "m7kP20nQbDRi")
+    @Test
+    void testLoginEndpoint() throws Exception {
+        MvcResult result = mockMvc.perform(post("/api/v2/auth")
+                                .with(httpBasic(username, password)))
+                                .andExpect(status().isOk())
+                                .andReturn();
+
+        String resultString = result.getResponse().getContentAsString();
+
+        assertNotNull(resultString);
+        assertFalse(resultString.isEmpty());
+    }
+}
