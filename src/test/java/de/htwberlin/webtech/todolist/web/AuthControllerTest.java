378230ebcbee0cf08231af37ca1e0df533b1c7d4
niklasbrecht <niklas.brecht@gmx.net>
2023-01-02 14:29:55 +0100

    added Fake Certs for Testreasons

--- src/test/java/de/htwberlin/webtech/todolist/web/AuthControllerTest.java
+++ src/test/java/de/htwberlin/webtech/todolist/web/AuthControllerTest.java
@@ -16,8 +16,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -49,15 +48,26 @@ public class AuthControllerTest {
 
     @WithMockUser(value = "test@gradle.com", password = "m7kP20nQbDRi")
     @Test
-    void testLoginEndpoint() throws Exception {
+    void testSuccessfullLogin() throws Exception {
         MvcResult result = mockMvc.perform(post("/api/v2/auth")
-                                .with(httpBasic(username, password)))
-                                .andExpect(status().isOk())
-                                .andReturn();
+                        .with(httpBasic(username, password)))
+                .andExpect(status().isOk())
+                .andReturn();
 
         String resultString = result.getResponse().getContentAsString();
 
         assertNotNull(resultString);
         assertFalse(resultString.isEmpty());
+    }
+
+    @Test
+    void testFailedLogin() throws Exception {
+        MvcResult result = mockMvc.perform(post("/api/v2/auth")
+                        .with(httpBasic("max@muster.is", "erfunden")))
+                        .andExpect(status().isUnauthorized())
+                        .andReturn();
+
+        String resultString = result.getResponse().getContentAsString();
+        assertTrue(resultString.isEmpty());
     }
 }
